9d165637e9c242bb1952a0ba593b4f8a05a1b384
abhishek <palabhishek322@gmail.com>
2018-05-14 00:57:01 +0530

    Testt

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
-    public class NinjectControllerFactory
+    public class NinjectControllerFactory : DefaultControllerFactory
     {
+        private IKernel kernel;
+
+        public NinjectControllerFactory()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)kernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            }.AsQueryable());
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
     }
 }
